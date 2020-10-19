cfe4c25646a6db699e9fa5516932538618bee759
+ <barboradujcikova@gmail.com>
2020-10-19 07:49:17 +0200

    -+-

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -4,9 +4,6 @@ namespace Open_Lab_03._11
 {
     public class Checker
     {
-        public bool IsPalindrome(string str)
-        {
-            throw new NotImplementedException();
-        } 
+        public bool IsPalindrome(string str) => str == new string(str.Reverse().ToArray());         
     }
 }
